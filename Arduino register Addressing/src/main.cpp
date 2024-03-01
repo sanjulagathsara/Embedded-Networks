9adc11b40dcd78f8bceba20c8793d9104fcee2e5
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2024-03-01 10:22:57 +0530

    fix

--- Arduino register Addressing/src/main.cpp
+++ Arduino register Addressing/src/main.cpp
@@ -8,9 +8,9 @@ void setup() {
   TCCR1A = 0; // Set entire TCCR1A register to 0
   TCCR1B = 0; // Same for TCCR1B
   TCNT1  = 0; // Initialize counter value to 0
-  
+
   // Set compare match register for 1hz increments
-  OCR1A = 15624; // = (16*10^6) / (1*1024) - 1 (must be <65536)
+  OCR1A = 15624/10; // = (16*10^6) / (1*1024) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS12 and CS10 bits for 1024 prescaler
@@ -20,13 +20,7 @@ void setup() {
   // enable global interrupts
   sei();
 
-
-
-
-
-
 }
-
 
 ISR(TIMER1_COMPA_vect){
   PORTB ^= (1<<PORTB5); // Toggle the LED
