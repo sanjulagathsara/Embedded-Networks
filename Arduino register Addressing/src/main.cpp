e6593210aeca506d8967aadaefdce603c07f8c54
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2024-03-01 09:59:15 +0530

    interrupt level delay toggle

--- Arduino register Addressing/src/main.cpp
+++ Arduino register Addressing/src/main.cpp
@@ -3,13 +3,35 @@
 void setup() {
   DDRB |= (1<<5); // Set PB5 as an output
   PORTB |= (1<<5); // Turn the LED on
+
+
+  TCCR1A = 0; // Set entire TCCR1A register to 0
+  TCCR1B = 0; // Same for TCCR1B
+  TCNT1  = 0; // Initialize counter value to 0
+  
+  // Set compare match register for 1hz increments
+  OCR1A = 15624; // = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  // enable global interrupts
+  sei();
+
+
+
+
+
+
+}
+
+
+ISR(TIMER1_COMPA_vect){
+  PORTB ^= (1<<PORTB5); // Toggle the LED
 }
 
 void loop() {
-
-  PORTB = 0b00000000; // Turn the LED off
-  delay(1000);
-  PORTB = 0b00100000; // Turn the LED on
-  delay(1000);
-
+  // put your main code here, to run repeatedly:
 }
